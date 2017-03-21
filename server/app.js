6211966a9cdcd5e475b47c758be2c9b5c2d1a2eb
EmilyH <EmilyHHLam@gmail.com>
2017-03-20 20:53:39 -0500

    solved the project

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var bodyParser = require( 'body-parser' );
 // create 'urlEncodedParser' in case we want to inject it for post calls:
 var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
 // use bodyParser.urlencoded throughout the app with this:
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 app.set("port", (process.env.PORT || 5000));
 
@@ -43,7 +43,25 @@ app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'server/public/views/index.html' ) );
 }); // end base url
 
-app.use( express.static( 'server/public/' ) );
+//routes to send data back to clients
+app.get ('/jokelist', function (req, res) {
+  console.log('jokes data sent');
+  res.send(jokes);
+
+});
+
+app.use( express.static( 'server/public' ) );
+
+//Rout Post request
+app.post('/newjoke', function(req, res) {
+
+  console.log('body=' + req.body);
+  jokes.push(req.body);
+  //console.log(newJoke);
+res.send(200);
+});
+
+
 
 app.listen( app.get("port"), function(){
   console.log( 'server up on: ', app.get("port") );
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,52 @@ console.log( 'js' );
 $( document ).ready( function(){
   console.log( 'JQ' );
   $( '#addJokeButton' ).on( 'click', function(){
+
+
     console.log( 'addJokeButton on click');
+
+
+    var joke = {
+        whoseJoke: $("#whoseJokeIn").val(),
+        jokeQuestion: $("#questionIn").val(),
+        punchLine: $("#punchlineIn").val()
+  };
+
+    console.log('joke = ' + joke);
+
+     $.ajax({
+       type: 'POST',
+       url:'/newjoke',
+       data: joke,
+      success: function(response) {
+       console.log("SUCCESS!!!");
+       console.log(response);
+       getJokes();
+     }
+
+  });
+
+
   }); // end addJokeButton on click
 }); // end doc ready
+
+function getJokes() {
+  $.ajax({
+    type: 'GET',
+    url: '/jokelist',
+    // response is the products array
+    success: function(response){
+    console.log(response);
+    displayJokes(response);
+  }
+});
+}
+
+function displayJokes(jokes) {
+  for (var i = 0; i< jokes.length; i++) {
+
+    $("#outputDiv").append('<div><b>Whose Joke</b>:' + jokes[i].whoseJoke
+    + '<br>Question:' + jokes[i].jokeQuestion + '<br>Punchline:' + jokes[i].punchLine + '</div><p></p>');
+  }
+
+}
